b0893884d841f3364e43844936d69627ef1557eb
d3vkk <donaldkk22@yahoo.com>
2023-01-23 13:56:47 +0300

    refactor: Add tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">React Tailwind Starter</div>;
+  return <div className="App text-4xl font-bold">React Tailwind Starter</div>;
 }
 
 export default App;
